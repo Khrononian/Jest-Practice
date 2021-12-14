64022b953b729e6856bd10f3cf5f6513399e55f6
Khrononian <Alv.Barnes@gmail.com>
2021-12-13 23:59:16 -0500

    Added new javascript file

---
+++ Code/analyzeArray.js
@@ -0,0 +1,12 @@
+function analyzeArray (array) {
+    const object = {
+        average: Math.round(array.reduce((a, b) => (a + b) / array.length)),
+        min: Math.min(...array),
+        max: Math.max(...array),
+        length: array.length
+    }
+
+    return object;
+}
+
+export default analyzeArray
